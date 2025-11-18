fdbf8f577eaff29c4ca3f7ad923a0363c9f81b40
Summit Haque <summit.haque@gmail.com>
2025-11-18 12:35:54 -0800

    In frontend/src/main/components/Nav/AppNavbar.js,add a section of code that establishes the navbar link for UCSBDiningCommonsMenuItem.

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -68,6 +68,9 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/ucsbdates">
                     UCSB Dates
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/ucsb-dining-commons-menu-items">
+                    UCSB Dining Commons Menu Items
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
